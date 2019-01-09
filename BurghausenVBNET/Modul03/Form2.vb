7701127c02970bef13631fa711d888928e286948
Hannes Preishuber <hannesp@ppedv.de>
2019-01-09 12:22:13 +0100

    String Replace

---
+++ BurghausenVBNET/Modul03/Form2.vb
@@ -0,0 +1,24 @@
+﻿Public Class Form2
+    Private Sub Form2_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As String
+        i = "1"
+        Const pi = 3.1415926
+
+        Dim d = 10.0R
+
+        Dim farbe = &HFF00FF
+        Dim b = &B10101010101
+
+        Dim s = "heute ist der: " +
+            Date.Now.ToString
+        'string literal
+        Dim heute = Date.Now.ToShortDateString
+        heute = "Mittowch"
+        Dim s1 = $"heute ist der {heute} Tag und pi ist noch immer {pi} {d:0000.00}" 'VB 14
+
+        'string formater
+        Dim neu = d.ToString("n9")
+
+
+    End Sub
+End Class
---
+++ BurghausenVBNET/Modul03/Form3.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form3
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Label1.Text = TextBox1.Text.Replace(" ", "-")
+
+    End Sub
+End Class
