aafbe963cd2c4104e753aaf30209fe16d496ec2b
Hannes Preishuber <hannesp@ppedv.de>
2019-01-09 16:24:47 +0100

    Seelct Case

---
+++ BurghausenVBNET/Modul04/Form9.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form9
+    Private Sub Form9_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim variableName As Integer
+        Select Case variableName
+            Case 1
+
+            Case 2 To 5
+
+            Case Else
+
+        End Select
+
+    End Sub
+End Class
