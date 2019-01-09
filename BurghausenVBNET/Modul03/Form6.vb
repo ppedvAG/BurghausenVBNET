e03e6d4c0c9c7743b6c5b333fe58e533ecd7f20c
Hannes Preishuber <hannesp@ppedv.de>
2019-01-09 15:14:29 +0100

    Nullable

---
+++ BurghausenVBNET/Modul03/Form6.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form6
+    Public i As Integer = 1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim i As Integer?
+        Dim o As Object
+        Dim b = IsNothing(i)
+        i = 12
+        i = Nothing
+
+        Dim s As String
+
+        Dim isnix = String.IsNullOrEmpty(s) '"" oder nothing
+
+
+    End Sub
+    Sub MySub()
+        ' i ist 1
+        i = 2
+
+    End Sub
+End Class
