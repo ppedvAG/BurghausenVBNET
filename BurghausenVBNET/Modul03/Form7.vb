ed6f5ca8ea2aab68cb822a4beb4f6e1383173207
Hannes Preishuber <hannesp@ppedv.de>
2019-01-09 15:38:08 +0100

    Arrays Basics

---
+++ BurghausenVBNET/Modul03/Form7.vb
@@ -0,0 +1,22 @@
+﻿Public Class Form7
+
+    Dim a(5) As String 'höchstes Element 
+
+    Dim anrede() As String = {"Frau", "Herr", "Firma"}
+
+    Dim zähler As Integer = 0
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        a(zähler) = Rnd().ToString
+        Dim x = a(5)
+        ListBox1.DataSource = Nothing
+        ListBox1.DataSource = a
+
+        zähler += 1
+
+    End Sub
+    Sub MySub()
+        ReDim a(10)
+
+    End Sub
+End Class
--- BurghausenVBNET/My Project/Application.Designer.vb
+++ BurghausenVBNET/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BurghausenVBNET.Form6
+            Me.MainForm = Global.BurghausenVBNET.Form7
         End Sub
     End Class
 End Namespace
