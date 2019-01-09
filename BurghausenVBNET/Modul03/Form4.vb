b1a2db6a00884afc2977c73c466b1a4ca645fdab
Hannes Preishuber <hannesp@ppedv.de>
2019-01-09 14:10:44 +0100

    Daten konvertierung Taschenrechner

---
+++ BurghausenVBNET/Modul03/Form4.vb
@@ -0,0 +1,12 @@
+﻿Public Class Form4
+    Private Sub Form4_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        ' Label1.Text = (CDbl(TextBox1.Text) + Double.Parse(TextBox2.Text)).ToString
+
+        Label1.Text = CStr(CDbl(TextBox1.Text) + Double.Parse(TextBox2.Text))
+
+    End Sub
+End Class
