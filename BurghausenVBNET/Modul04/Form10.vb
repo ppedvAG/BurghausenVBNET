0b2602add8c6f0c62947105367bd038d57bbafb7
Hannes Preishuber <hannesp@ppedv.de>
2019-01-09 16:42:02 +0100

    For Next Lotto Übung

---
+++ BurghausenVBNET/Modul04/Form10.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form10
+    Private Sub Form10_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim a(5) As Integer
+
+        For i = 0 To 5 Step 1
+            a(i) = CInt(Rnd() * 49) + 1
+        Next
+        ListBox1.DataSource = a
+    End Sub
+End Class
