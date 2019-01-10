5b9c0fb8cf5399bbb2580eb8366bc2e23f9dfd8f
Hannes Preishuber <hannesp@ppedv.de>
2019-01-10 15:09:08 +0100

    Liste Prozesse LINQ

---
+++ BurghausenVBNET/Modul06/Form19.vb
@@ -0,0 +1,14 @@
+﻿Public Class Form19
+    Private Sub Form19_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim procList As List(Of Process) = Diagnostics.Process.GetProcesses.ToList
+
+
+        Dim q = From pc In procList
+                Order By pc.PagedMemorySize64
+                Select pc.ProcessName
+
+        ListBox1.DataSource = q.ToArray
+
+
+    End Sub
+End Class
--- BurghausenVBNET/My Project/Application.Designer.vb
+++ BurghausenVBNET/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BurghausenVBNET.Form18
+            Me.MainForm = Global.BurghausenVBNET.Form19
         End Sub
     End Class
 End Namespace
