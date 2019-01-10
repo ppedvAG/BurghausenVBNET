4407ccd89661abd4816b292965ed6900a18e4cd6
Hannes Preishuber <hannesp@ppedv.de>
2019-01-10 14:47:21 +0100

    Übung Filter auf Liste mit LINQ und Lambda Expression

---
+++ BurghausenVBNET/Modul06/Form18.vb
@@ -0,0 +1,11 @@
+﻿Public Class Form18
+    Dim hersteller As New List(Of String) From {"BMW", "AUDI", "VW", "FORD", "Mazda", "Opel", "Renault", "GM", "Tesla", "Dacia", "Mercedes"}
+    Private Sub Form18_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        ListBox1.DataSource = hersteller
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+        ListBox1.DataSource = hersteller.Where(Function(x) x.ToLower.Contains(TextBox1.Text.ToLower)).ToList
+    End Sub
+End Class
