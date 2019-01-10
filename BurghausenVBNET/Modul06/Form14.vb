2ec729a141f5087644783d01b091554e4e2db966
Hannes Preishuber <hannesp@ppedv.de>
2019-01-10 11:29:43 +0100

    Enum

---
+++ BurghausenVBNET/Modul06/Form14.vb
@@ -0,0 +1,17 @@
+﻿Public Class Form14
+    Private Sub Form14_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i = Anrede.Firma.ToString
+        Dim a = System.Enum.GetValues(GetType(Anrede))
+
+
+        MessageBox.Show(Nothing, "Message", "Überschrift",
+        MessageBoxButtons.OKCancel, MessageBoxIcon.Hand)
+
+
+    End Sub
+    Enum Anrede
+        Herr = 1
+        Frau = 5
+        Firma=10
+    End Enum
+End Class
