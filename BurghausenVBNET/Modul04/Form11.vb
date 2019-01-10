9abf93a3e216eef23e2e3dc69f7718521768a504
Hannes Preishuber <hannesp@ppedv.de>
2019-01-10 09:22:18 +0100

    Übung halber Weihnachstbaum

---
+++ BurghausenVBNET/Modul04/Form11.vb
@@ -0,0 +1,15 @@
+﻿Public Class Form11
+    Private Sub Form11_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim baum(9) As String
+        Dim vieleSternchen = "***********"
+        For schleifenzähler = 0 To 9
+            If schleifenzähler < 7 Then
+                baum(schleifenzähler) = vieleSternchen.Substring(0, schleifenzähler + 1)
+            Else
+                baum(schleifenzähler) = "*"
+            End If
+        Next
+
+        ListBox1.DataSource = baum
+    End Sub
+End Class
--- BurghausenVBNET/My Project/Application.Designer.vb
+++ BurghausenVBNET/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.BurghausenVBNET.Form10
+            Me.MainForm = Global.BurghausenVBNET.Form11
         End Sub
     End Class
 End Namespace
