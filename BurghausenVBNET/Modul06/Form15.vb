5733effd6545e449e0c0efc7cf9e41f14457ecff
Hannes Preishuber <hannesp@ppedv.de>
2019-01-10 11:43:28 +0100

    Arraylist

---
+++ BurghausenVBNET/Modul06/Form15.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form15
+    Private Sub Form15_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim a As ArrayList = New ArrayList()
+        a.Add("Hannes")
+        a.Add(1)
+        a.Add(0)
+        a.Add({"Hannes", "Preishuber"})
+        If a.Count > 2 Then
+            a.Item(0) = "dfdfsdf"
+            a(0) = "sfdsd"
+            a.Remove(0) 'Wert
+            a.RemoveAt(0) 'index
+            a.Insert(4, "Wert")
+            If a.Contains("Hannes") Then
+
+            End If
+        End If
+
+
+    End Sub
+End Class
