cab667de12a7487a95d50200738dbb4996836479
Hannes Preishuber <hannesp@ppedv.de>
2019-01-11 15:16:38 +0100

    Parser Orte StreamReader

---
+++ BurghausenVBNET/modul10/Form29.vb
@@ -0,0 +1,29 @@
+﻿Imports System.IO
+
+Public Class Form29
+    Dim liste As New List(Of Orte)
+    Private Sub Form29_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim datei = "C:\vb.net\BurghausenVBNET\BurghausenVBNET\modul10\germany_de.csv"
+        Try
+            Using sr = New StreamReader(datei)
+                sr.ReadLine()
+
+                While sr.Peek >= 0
+                    Dim s = sr.ReadLine
+                    s = s.Replace("""", "")
+                    Dim a = s.Split(CType(";", Char()))
+                    Dim ort As New Orte
+                    ort.Plz = CInt(a(3))
+                    ort.Name = a(4)
+                    ort.Longitude = CDbl(a(16))
+                    ort.Latitude = CDbl(a(17))
+                    liste.Add(ort)
+                End While
+            End Using
+
+        Catch ex As Exception
+            MessageBox.Show("Fehler beim lesen der Datei Germany_de.csv")
+            Liste.Add(New Orte With {.Name="Importfehler"})
+        End Try
+    End Sub
+End Class
---
+++ BurghausenVBNET/modul10/orte.vb
@@ -0,0 +1,6 @@
+﻿Public Class Orte
+    Public Property Plz As Integer
+    Public Property Name As String
+    Public Property Longitude As Double
+    Public Property Latitude As Double
+End Class
