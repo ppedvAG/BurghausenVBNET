718c589c6f5112a2e3fdb94bff4a37d44b1518cf
Hannes Preishuber <hannesp@ppedv.de>
2019-01-11 11:59:49 +0100

    Extenson Methode

---
+++ BurghausenVBNET/Modul07/Form27.vb
@@ -0,0 +1,8 @@
+﻿Public Class Form27
+    Private Sub Form27_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim i As Integer = 23423423
+        Dim d = i.ToDate
+
+        Dim dd = Module1.ToDate(i) 'kompiler macht daraus
+    End Sub
+End Class
---
+++ BurghausenVBNET/Modul07/Module1.vb
@@ -0,0 +1,9 @@
+﻿Imports System.Runtime.CompilerServices
+
+Module Module1
+    <Extension()>
+    Public Function ToDate(wert As Integer) As Date
+        Return Date.Parse("01/01/1970").AddSeconds(wert)
+    End Function
+
+End Module
