a8915129dcda155c5a5d9c8a4114939b8ea4664d
Hannes Preishuber <hannesp@ppedv.de>
2019-01-11 11:50:57 +0100

    Interface

---
+++ BurghausenVBNET/Modul07/Class1.vb
@@ -0,0 +1,16 @@
+﻿Imports BurghausenVBNET
+
+Public Class Class1
+    Implements IDisposable, ppedvInterface
+
+    Public Sub Dispose() Implements IDisposable.Dispose
+        Throw New NotImplementedException()
+    End Sub
+
+    Public Function Add(i As Integer) As Integer Implements ppedvInterface.Add
+        Throw New NotImplementedException()
+    End Function
+End Class
+Interface ppedvInterface
+    Function Add(i As Integer) As Integer
+End Interface
---
+++ BurghausenVBNET/Modul07/Form26.vb
@@ -0,0 +1,7 @@
+﻿Public Class Form26
+    Private Sub Form26_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Using u As New Class1
+            u.Add(1)
+        End Using
+    End Sub
+End Class
