0fde0ae0fe134261215950752c1779e55c94bc20
Hannes Preishuber <hannesp@ppedv.de>
2019-01-11 10:29:06 +0100

    Using, Anoymes Objekt

---
+++ BurghausenVBNET/Modul07/Form23.vb
@@ -0,0 +1,17 @@
+﻿Imports System.IO
+
+Public Class Form23
+    Private Sub Form23_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim r = New Rechnung
+        Dim rr = New With {.ID = 1, .Text = "Text"}
+
+        Using writer As TextWriter = File.CreateText("log.txt")
+            Dim i = 1
+            writer.WriteLine("This is line one.")
+            writer.WriteLine("This is line two.")
+        End Using
+
+        Dim writer2 As TextWriter = File.CreateText("log.txt")
+
+    End Sub
+End Class
--- BurghausenVBNET/Modul07/Rechnung.vb
+++ BurghausenVBNET/Modul07/Rechnung.vb
@@ -1,20 +1,21 @@
 ﻿Public Class Rechnung
     Public Property Kunde As String
-    Public Property Summe As Integer
+    Public ReadOnly Property Summe As Integer
     Public Property RechnungPositionen() As List(Of Positionen)
     Public Sub New()
         _RechnungPositionen = New List(Of Positionen)
     End Sub
     Public Sub AddPosition(p As Positionen)
         Dim max As Integer
-        If RechnungPositionen.Count > 0 Then
-            max = _rechnungPositionen.Max(Function(x) x.Id)
+        If _RechnungPositionen.Count > 0 Then
+            max = _RechnungPositionen.Max(Function(x) x.Id)
         Else
             max = 0
         End If
         p.Id = max + 1
-        RechnungPositionen.Add(p)
-        Summe = _rechnungPositionen.Sum(Function(r) r.Anzahl * r.Preis)
+        _RechnungPositionen.Add(p)
+        _Summe = _RechnungPositionen.Sum(Function(r) r.Anzahl * r.Preis)
 
     End Sub
+
 End Class
